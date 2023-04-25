cf6710c041c4074560d3900a77ac329869350eea
UGRAMRAJU <ugram.raju@gmail.com>
2023-04-25 17:45:38 +0530

    Update userRoutes.js

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -5,46 +5,45 @@ const jwt = require("jsonwebtoken");
 const middleware = require("../MiddleWare/middleWare")
 const router = express.Router();
 
-//user Registeration
+//user Registration
 router.post("/registration",async(req,res)=>{
     try{
         const{username,password,confirmpassword} = req.body;
         let exit = await Registeruser.findOne({username});
         if(exit){
-            return res.status(400).send("User Already Exists")
+            return res.status(400).json({ error: "User Already Exists" });
         }
         if(password !== confirmpassword){
-            return res.status(400).send("Passwords are not Matched")
+            return res.status(400).json({ error: "Passwords are not Matched" });
         }
 
-        //hasshing password
-        const hassedPassword = await bcrypt.hash(password,10);
+        //hashing password
+        const hashedPassword = await bcrypt.hash(password,10);
 
-        let newUser =await Registeruser.create({
+        let newUser = await Registeruser.create({
             username,
-            password:hassedPassword,
-            confirmpassword:hassedPassword
-        })
-        res.status(200).json({newUser, msg:"User Registered Successfully"})
+            password: hashedPassword,
+            confirmpassword: hashedPassword
+        });
+        res.status(200).json({ newUser, message: "User Registered Successfully" });
     }
     catch(err){
         console.log(err);
-        res.status(500).send("Internal Server Errors")
+        res.status(500).json({ error: "Internal Server Error" });
     }
-})
+});
 
 //user Login
-
 router.post("/login",async(req,res)=>{
     try{
         const {username,password} = req.body;
         let exit = await Registeruser.findOne({username});
         if(!exit){
-            return res.status(400).send("User Not Exists")
+            return res.status(400).json({ error: "User Not Exists" });
         }
         const isMatch = await bcrypt.compare(password, exit.password);
         if(!isMatch){
-            return res.status(400).send("Invalid Password")
+            return res.status(400).json({ error: "Invalid Password" });
         }
         let payload={
             user:{
@@ -54,29 +53,28 @@ router.post("/login",async(req,res)=>{
         jwt.sign(payload,process.env.access_token_secreat,{expiresIn:360000},
             (err,token)=>{
                 if(err) throw err;
-                return res.json({token,msg:"Login Successfully"})
+                return res.json({ token, message: "Login Successfully" });
             })
     }
     catch(err){
         console.log(err);
-        res.status(500).send("Internal Server Errors")
+        res.status(500).json({ error: "Internal Server Error" });
     }
-
-})
+});
 
 //particular user get in
 router.get("/currentuser",middleware, async(req,res)=>{
     try{
         const user = await Registeruser.findById(req.user.id)
         if(!user){
-            return res.status(400).send("user Not Available")
+            return res.status(400).json({ error: "User Not Available" });
         }
-       return res.json({user})
+       return res.json({ user });
     }
     catch(err){
         console.log(err);
-        res.status(500).send("Internal Server Errors")
+        res.status(500).json({ error: "Internal Server Error" });
     }
-})
+});
 
 module.exports = router;
